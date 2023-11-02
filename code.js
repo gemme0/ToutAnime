1e3c7f1d53cb826bceb513ffdfc1e6b1759cce48
gemme0 <137955466+gemme0@users.noreply.github.com>
2023-11-03 00:26:53 +0100

    Update code.js

--- code.js
+++ code.js
@@ -30,3 +30,20 @@ optionsList.forEach(o => {
     optionsContainer.classList.remove("active");
   });
 });
+
+$(document).ready(function(){
+  $('.button').click(function(){
+    if($(this).hasClass('expand')){
+      $('ul').slideUp(function(){
+        $('.button').removeClass('expand');
+        $('.fas').removeClass('expand')
+      });
+    }else{
+      $(this).addClass('expand');
+      setTimeout(function(){
+        $('.fas').addClass('expand');
+        $('ul').stop().slideDown();
+      },200);
+    }
+  });
+});
